97a556e062f000cab2f8d486e1a13cb3579d5c21
WoongJiKang <121491959+woong020477@users.noreply.github.com>
2024-11-11 01:31:42 +0900

    Project Setting
    프로젝트 생성 및 스타터 컨텐츠 에셋 추가
    (JunkYard, OldMine, TemplesofCambodia 는 개별설치해야함)

---
+++ Source/Escape_Mine/Private/Escape_Mine.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Escape_Mine.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Escape_Mine, "Escape_Mine" );
